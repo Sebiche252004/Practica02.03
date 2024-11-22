6156e7d2721f559c10427454ecee163b87639eca
Sebiche252004 <svelasqpum@educacion.navarra.es>
2024-11-22 01:01:16 +0100

    Create practica0203_ej2.py

---
+++ practica0203_ej2.py
@@ -0,0 +1,10 @@
+#divisor
+
+dividendo=int(input("introduzca un dividendo"))
+divisor=int(input("introduzca un divisor"))
+
+if divisor==0:
+    print("Error")
+else:
+    resultado=dividendo/divisor
+    print("El resultado es", resultado)
