82c175e26f21837e5a558becb2fa2ed4ece42d65
Sebiche252004 <svelasqpum@educacion.navarra.es>
2024-11-22 01:00:33 +0100

    Create Practica02.03_ej1.py

---
+++ Practica02.03_ej1.py
@@ -0,0 +1,11 @@
+# contraseña
+
+contraseña= input("Introduzca una contraseña")
+contraseña_pedir= input("Introduzca la contraseña")
+
+contraseña_pedir2=contraseña_pedir.lower()
+
+if contraseña==contraseña_pedir2:
+    print("Contraseña correcta")
+else:
+    print("contraseña incorrecta")
